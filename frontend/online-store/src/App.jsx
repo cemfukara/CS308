63fcf73206a5ce469d89d66cc0df7b0d3eab79e3
Furkan02124 <furkan.eren@sabanciuniv.edu>
2025-11-05 01:17:34 +0300

    Add FontAwesome icons and toast notifications to ProductDetails page; update styles and structure

--- frontend/online-store/src/App.jsx
+++ frontend/online-store/src/App.jsx
@@ -7,22 +7,26 @@ import Auth from './pages/Auth';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Cart from './pages/Cart';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
+    <>
+      <Router>
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path={`/products/:id`} element={<ProductDetails />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path={`/products/:id`} element={<ProductDetails />} />
+        </Routes>
 
-      <Footer />
-    </Router>
+        <Footer />
+      </Router>
+      <Toaster position="top-right" />
+    </>
   );
 }
 
