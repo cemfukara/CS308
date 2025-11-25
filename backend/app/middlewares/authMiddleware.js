7d707626fcd2b6cf971a16a8142e8f62b2f8b251
tahaaa86 <taha.guler@sabanciuniv.edu>
2025-11-25 11:49:49 +0300

    Added AUTH_DISABLED field to .env.example that disables all the authentication middleware

--- backend/app/middlewares/authMiddleware.js
+++ backend/app/middlewares/authMiddleware.js
@@ -3,39 +3,51 @@
 import jwt from 'jsonwebtoken';
 
 export const isAuthenticated = (req, res, next) => {
-    const token = req.cookies.token; // read from cookie
+  if (
+    process.env.AUTH_DISABLED === 'true' &&
+    process.env.NODE_ENV == 'development'
+  ) {
+    req.user = {
+      user_id: 0,
+      email: 'dev@test.local',
+      role: 'dev',
+    };
+    return next();
+  }
+  const token = req.cookies.token; // read from cookie
 
-    if (!token)
-        return res
-            .status(401)
-            .json({ message: 'Unauthorized: No token provided' });
+  if (!token)
+    return res.status(401).json({ message: 'Unauthorized: No token provided' });
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET); //try to verify token
-        req.user = decoded;
-        next();
-    } catch {
-        res.status(401).json({ message: 'Invalid or expired token' });
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); //try to verify token
+    req.user = decoded;
+    return next();
+  } catch {
+    res.status(401).json({ message: 'Invalid or expired token' });
+  }
 };
 
 // check auth for specified roles. "allowedRoles" could be "admin", "user", "product_manager" etc.
 export const authorizeRoles = (allowedRoles = []) => {
-    return (req, res, next) => {
-        const user = req.user;
+  return (req, res, next) => {
+    const user = req.user;
 
-        if (!user) {
-            return res.status(401).json({ message: 'Unauthorized' });
-        }
+    if (!user) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
 
-        if (!allowedRoles.includes(user.role)) {
-            return res
-                .status(403)
-                .json({ message: 'Forbidden: Access denied.' });
-        }
+    // If in dev mode with no authentication, allow "dev" role to access anything
+    if (process.env.NODE_ENV === 'development' && user.role === 'dev') {
+      return next();
+    }
 
-        next();
-    };
+    if (!allowedRoles.includes(user.role)) {
+      return res.status(403).json({ message: 'Forbidden: Access denied.' });
+    }
+
+    return next();
+  };
 };
 
 export const isAdmin = authorizeRoles(['product manager']);
