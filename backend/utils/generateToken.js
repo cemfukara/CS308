1ab8afc6fcbf2d1cc60af26215ce0c25504cd33a
tahaaa86 <taha.guler@sabanciuniv.edu>
2025-11-28 01:11:56 +0300

    Implemented update profile route: PATCH /users/profile
    Updated API_DOCUMENTATION: Added PATCH /users/profile, Added JWT token payload contents
    
    Seperated JWT token generator to independent file

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,1 +1,21 @@
 // Helper function to generate JWT tokens for user authentication.
+import jwt from 'jsonwebtoken';
+
+const ACCESS_TOKEN_SECRET = process.env.JWT_SECRET;
+
+// Usually tokens expire in 1h, but as refresh tokens are not implemented yet, larger expiry is set.
+const ACCESS_TOKEN_EXPIRES_IN = '7d';
+
+export const generateAccessToken = (payload) => {
+  const payloadToBeSent = {
+    user_id: payload.user_id,
+    email: payload.email,
+    role: payload.role,
+  };
+  console.log(`generating token ${payloadToBeSent.email}`);
+
+  // Sign
+  return jwt.sign(payloadToBeSent, ACCESS_TOKEN_SECRET, {
+    expiresIn: ACCESS_TOKEN_EXPIRES_IN,
+  });
+};
